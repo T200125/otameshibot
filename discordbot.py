a149fd1c072824f572c28dde9393a7fd875591ec
T200125 <80594110+T200125@users.noreply.github.com>
2021-03-14 14:45:50 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -5,6 +5,29 @@ import traceback
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
+dateTimeList1 = [
+'2021/03/14 14:48',
+'2021/03/16 20:00',
+'2021/03/18 20:00',
+'2021/03/20 20:00',
+'2021/03/22 20:00',
+'2021/03/24 20:00',
+'2021/03/26 20:00', 
+'2021/03/28 20:00',
+'2021/03/30 20:00',
+]
+
+dateTimeList2 = [
+'2021/03/15 20:00',
+'2021/03/17 20:00',
+'2021/03/19 20:00',
+'2021/03/21 20:00',
+'2021/03/23 20:00',
+'2021/03/25 20:00',
+'2021/03/27 20:00', 
+'2021/03/29 20:00',
+'2021/03/31 20:00',
+]
 
 @bot.event
 async def on_command_error(ctx, error):
@@ -13,9 +36,22 @@ async def on_command_error(ctx, error):
     await ctx.send(error_msg)
 
 
+
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
 
+@tasks.loop(seconds=60)
+async def time_check():
+    sleepTime = 0
+    # 現在の時刻
+    now = datetime.now().strftime('%Y/%m/%d %H:%M')
+    if now in dateTimeList1 :
+        print(now)
+        await print(本日のギルドマイレージは\n【パターン１】\nラモー戦場１回入場（ヴォルクス）\nラモー戦場１回入場（黒結晶）\nラモー戦場２回入場（ヴォルクス）\nラモー戦場２回入場（黒結晶）\n古代遺跡５回完了\n古代遺跡１５回完了)
+
+
+#ループ処理実行
+loop.start()
 
 bot.run(token)
