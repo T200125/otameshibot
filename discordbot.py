c9d342f525a90ddb9be2fea89ab5ac1fad446464
T200125 <80594110+T200125@users.noreply.github.com>
2021-03-15 02:24:05 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -11,7 +11,7 @@ channel = os.environ['CHANNEL_ID']
 client = discord.Client()
 
 
-dateTimeList_1 = ['2021/03/15 02:08', '2021/03/17 20:00', '2021/03/19 20:00', '2021/03/21 20:00', '2021/03/23 20:00', '2021/03/25 20:00', '2021/03/27 20:00', '2021/03/29 20:00', '2021/03/31 20:00']
+dateTimeList_1 = ['2021/03/15 02:25', '2021/03/17 20:00', '2021/03/19 20:00', '2021/03/21 20:00', '2021/03/23 20:00', '2021/03/25 20:00', '2021/03/27 20:00', '2021/03/29 20:00', '2021/03/31 20:00']
 
 dateTimeList_2 = ['2021/03/16 20:00', '2021/03/18 20:00', '2021/03/20 20:00', '2021/03/22 20:00', '2021/03/24 20:00', '2021/03/26 20:00', '2021/03/28 20:00', '2021/03/30 20:00']
 
@@ -26,42 +26,36 @@ async def on_command_error(ctx, error):
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
-    
+
 
 async def SendMessage_1():
     await channel.send('@everyone\n本日のギルドマイレージは\n【パターン２】\n闘技場へ１回入場\n闘技場へ２回入場\n薬草を５回採集する\n石を５回採鉱する\n木を５回伐採する\n古代遺跡１０回完了')
-    
+
+
 async def SendMessage_2():
     await channel.send('@everyone\n本日のギルドマイレージは\n【パターン１】\nラモー戦場１回入場（ヴォルクス）\nラモー戦場１回入場（黒結晶）\nラモー戦場２回入場（ヴォルクス）\nラモー戦場２回入場（黒結晶）\n古代遺跡５回完了\n古代遺跡１５回完了')
 
 
 @tasks.loop(seconds=60)
-async def time_check():
-    sleepTime = 0
-    # 現在の時刻
+async def time_check_1():
+
     now = datetime.now().strftime('%Y/%m/%d %H:%M')
-    if now in dateTimeList_1 :
+    if now in dateTimeList_1:
         print(now)
         await SendMessage_1()
 
-        await asyncio.sleep(86100)
-        
+        await async.sleep(60)
+
 @tasks.loop(seconds=60)
-async def time_check():
-    sleepTime = 0
-    # 現在の時刻
+async def time_check_2():
+
     now = datetime.now().strftime('%Y/%m/%d %H:%M')
-    if now in dateTimeList_2 :
+    if now in dateTimeList_2:
         print(now)
         await SendMessage_2()
 
-        await asyncio.sleep(86100)
-
-
-
+        await async.sleep(60)
 
 time_check.start()
-
-
 
 bot.run(token)
