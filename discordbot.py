bc47b29e3a21bf78422f6074ae290a35b6521638
T200125 <80594110+T200125@users.noreply.github.com>
2021-03-16 21:30:01 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,16 +1,15 @@
 from discord.ext import commands
 import os
 import traceback
+from discord.ext import tasks
 from datetime import datetime
-import time
+import asyncio
 
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
-channel = 820269526733160451
+channel = guild.get_channel(820269526733160451)
 
-dateTimeList_1 = ['2021/03/15 22:48', '2021/03/17 20:00', '2021/03/19 20:00', '2021/03/21 20:00', '2021/03/23 20:00', '2021/03/25 20:00', '2021/03/27 20:00', '2021/03/29 20:00', '2021/03/31 20:00']
-
-dateTimeList_2 = ['2021/03/16 20:00', '2021/03/18 20:00', '2021/03/20 20:00', '2021/03/22 20:00', '2021/03/24 20:00', '2021/03/26 20:00', '2021/03/28 20:00', '2021/03/30 20:00']
+dateTimeList = ['2021/03/17 20:00', '2021/03/19 20:00', '2021/03/21 20:00', '2021/03/23 20:00', '2021/03/25 20:00', '2021/03/27 20:00', '2021/03/29 20:00', '2021/03/31 20:00', '2021/03/16 21:35', '2021/03/18 20:00', '2021/03/20 20:00', '2021/03/22 20:00', '2021/03/24 20:00', '2021/03/26 20:00', '2021/03/28 20:00', '2021/03/30 20:00']
 
 print("確認0")
 
@@ -22,46 +21,28 @@ async def on_command_error(ctx, error):
 
 print("確認")
 
-def SendMessage_1():
-    channel.send('@everyone\n本日のギルドマイレージは\n【パターン２】\n闘技場へ１回入場\n闘技場へ２回入場\n薬草を５回採集する\n石を５回採鉱する\n木を５回伐採する\n古代遺跡１０回完了')
-
-
-def SendMessage_2():
-    channel.send('@everyone\n本日のギルドマイレージは\n【パターン１】\nラモー戦場１回入場（ヴォルクス）\nラモー戦場１回入場（黒結晶）\nラモー戦場２回入場（ヴォルクス）\nラモー戦場２回入場（黒結晶）\n古代遺跡５回完了\n古代遺跡１５回完了')
-
-
 bot.run(token)
 
-
-def time_check_1():
+tasks.loop(seconds=5)
+async def time_check():
 
     print("確認１")
+
     now = datetime.now().strftime('%Y/%m/%d %H:%M')
 
     print("確認２")
-    if now in dateTimeList_1:
-        print(now)
-        SendMessage_1()
 
-        time.sleep(30)
+    if now in dateTimeList:
+	if datetime.now().day % 2 == 0
+
+	    print(now)
+	    message.channel.send('@everyone\n本日のギルドマイレージは\n【パターン１】\nラモー戦場１回入場（ヴォルクス）\nラモー戦場１回入場（黒結晶）\nラモー戦場２回入場（ヴォルクス）\nラモー戦場２回入場（黒結晶）\n古代遺跡５回完了\n古代遺跡１５回完了')
+	    await asyncio.sleep(60)
+	else:
+	    print(now)
+	    massage.channel.send('@everyone\n本日のギルドマイレージは\n【パターン２】\n闘技場へ１回入場\n闘技場へ２回入場\n薬草を５回採集する\n石を５回採鉱する\n木を５回伐採する\n古代遺跡１０回完了')
+	    await asyncio.sleep(60)
     else:
-        print("確認３")
+        return
 
-def time_check_2():
-
-    now = datetime.now().strftime('%Y/%m/%d %H:%M')
-    if now in dateTimeList_2:
-        print(now)
-        SendMessage_2()
-
-        time.sleep(87900)
-
-
-month = 0
-
-
-while month < 4:
-    time_check_1
-    time_check_2
-    now_check = datetime.today()
-    month = now_check.month
+asyncio.run(time_check())
