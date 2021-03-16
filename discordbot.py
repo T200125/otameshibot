ae01eeda5368e4cdbe350aba86fd50dc805ef15c
T200125 <80594110+T200125@users.noreply.github.com>
2021-03-16 21:35:35 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -33,15 +33,15 @@ async def time_check():
     print("確認２")
 
     if now in dateTimeList:
-	if datetime.now().day % 2 == 0
+        if datetime.now().day % 2 == 0
 
-		print(now)
-		message.channel.send('@everyone\n本日のギルドマイレージは\n【パターン１】\nラモー戦場１回入場（ヴォルクス）\nラモー戦場１回入場（黒結晶）\nラモー戦場２回入場（ヴォルクス）\nラモー戦場２回入場（黒結晶）\n古代遺跡５回完了\n古代遺跡１５回完了')
-		await asyncio.sleep(60)
-	else:
-		print(now)
-		massage.channel.send('@everyone\n本日のギルドマイレージは\n【パターン２】\n闘技場へ１回入場\n闘技場へ２回入場\n薬草を５回採集する\n石を５回採鉱する\n木を５回伐採する\n古代遺跡１０回完了')
-		await asyncio.sleep(60)
+            print(now)
+            message.channel.send('@everyone\n本日のギルドマイレージは\n【パターン１】\nラモー戦場１回入場（ヴォルクス）\nラモー戦場１回入場（黒結晶）\nラモー戦場２回入場（ヴォルクス）\nラモー戦場２回入場（黒結晶）\n古代遺跡５回完了\n古代遺跡１５回完了')
+            await asyncio.sleep(60)
+        else:
+            print(now)
+            massage.channel.send('@everyone\n本日のギルドマイレージは\n【パターン２】\n闘技場へ１回入場\n闘技場へ２回入場\n薬草を５回採集する\n石を５回採鉱する\n木を５回伐採する\n古代遺跡１０回完了')
+            await asyncio.sleep(60)
     else:
         return
 
