aba7247a93e6a1fb187a06ecff19a9b04c0bfd11
T200125 <80594110+T200125@users.noreply.github.com>
2021-03-18 18:11:13 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -4,6 +4,7 @@ import traceback
 from discord.ext import tasks
 from datetime import datetime
 import asyncio
+import discord
 
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
