70e1a57163699bdaafda907f5790db6c848e2e3d
T200125 <80594110+T200125@users.noreply.github.com>
2021-03-18 19:23:29 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -35,16 +35,16 @@ async def time_check():
 
     if now in dateTimeList:
 
-        channel.send('loop実行中')
+        await channel.send('loop実行中')
 
         if datetime.now().day % 2 == 0:
 
             print(now)
-            channel.send('@everyone\n本日のギルドマイレージは\n【パターン１】\nラモー戦場１回入場（ヴォルクス）\nラモー戦場１回入場（黒結晶）\nラモー戦場２回入場（ヴォルクス）\nラモー戦場２回入場（黒結晶）\n古代遺跡５回完了\n古代遺跡１５回完了')
+            await channel.send('@everyone\n本日のギルドマイレージは\n【パターン１】\nラモー戦場１回入場（ヴォルクス）\nラモー戦場１回入場（黒結晶）\nラモー戦場２回入場（ヴォルクス）\nラモー戦場２回入場（黒結晶）\n古代遺跡５回完了\n古代遺跡１５回完了')
             await asyncio.sleep(60)
         else:
             print(now)
-            channel.send('@everyone\n本日のギルドマイレージは\n【パターン２】\n闘技場へ１回入場\n闘技場へ２回入場\n薬草を５回採集する\n石を５回採鉱する\n木を５回伐採する\n古代遺跡１０回完了')
+            await channel.send('@everyone\n本日のギルドマイレージは\n【パターン２】\n闘技場へ１回入場\n闘技場へ２回入場\n薬草を５回採集する\n石を５回採鉱する\n木を５回伐採する\n古代遺跡１０回完了')
             await asyncio.sleep(60)
 
 time_check.start()
