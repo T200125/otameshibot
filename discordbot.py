c5b2506667bb75c3a342f6eb5caf8f9a4de5e2be
T200125 <80594110+T200125@users.noreply.github.com>
2021-03-19 20:13:37 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -11,7 +11,7 @@ bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
 # UTC
-dateTimeList = ['2021/03/18 11:45', '2021/03/19 11:15', '2021/03/21 11:00', '2021/03/23 11:00', '2021/03/25 11:00', '2021/03/27 11:00', '2021/03/29 11:00', '2021/03/31 11:00', '2021/03/18 11:00', '2021/03/20 11:00', '2021/03/22 11:00', '2021/03/24 11:00', '2021/03/26 11:00', '2021/03/28 11:00', '2021/03/30 11:00']
+dateTimeList = ['2021/03/18 11:45', '2021/03/19 11:00', '2021/03/21 11:00', '2021/03/23 11:00', '2021/03/25 11:00', '2021/03/27 11:00', '2021/03/29 11:00', '2021/03/31 11:00', '2021/03/18 11:00', '2021/03/20 11:00', '2021/03/22 11:00', '2021/03/24 11:00', '2021/03/26 11:00', '2021/03/28 11:00', '2021/03/30 11:00']
 
 
 @bot.event
@@ -24,21 +24,6 @@ async def on_command_error(ctx, error):
 async def time_check():
 
     now = datetime.now().strftime('%Y/%m/%d %H:%M')
-# 動作確認用
-    channel = bot.get_channel(820269526733160451)
-
-    if now in dateTimeList:
-        print("loopcheck2")
-        if datetime.now().day % 2 == 0:
-
-            print(now)
-            await channel.send('@Slaughter\n本日のギルドマイレージは\n【パターン１】\nラモー戦場１回入場（ヴォルクス）\nラモー戦場１回入場（黒結晶）\nラモー戦場２回入場（ヴォルクス）\nラモー戦場２回入場（黒結晶）\n古代遺跡５回完了\n古代遺跡１５回完了')
-            await asyncio.sleep(60)
-        else:
-            print(now)
-            await channel.send('@Slaughter\n本日のギルドマイレージは\n【パターン２】\n闘技場へ１回入場\n闘技場へ２回入場\n薬草を５回採集する\n石を５回採鉱する\n木を５回伐採する\n古代遺跡１０回完了')
-            await asyncio.sleep(60)
-
     channel = bot.get_channel(639384765962649601)
 
     if now in dateTimeList:
