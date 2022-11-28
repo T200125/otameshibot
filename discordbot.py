bc807ccb84778727a4e4cd49e2e0d1358cffa22f
T200125 <80594110+T200125@users.noreply.github.com>
2022-11-28 23:54:00 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -10,7 +10,10 @@ intents = discord.Intents.default()
 intents.message_content = True
 
 client = discord.Client(intents=intents)
-bot = commands.Bot(command_prefix='*$')
+intents = discord.Intents.all()
+intents.members = True
+
+client = discord.Bot(command_prefix=prefix, case_insensitive=True, self_bot=True)
 token = os.environ['DISCORD_BOT_TOKEN']
 
 # UTC
