89d75fd62e7869cfa1e8a1400400272e027b9355
T200125 <80594110+T200125@users.noreply.github.com>
2022-12-04 20:37:54 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -6,12 +6,17 @@ from datetime import datetime
 import asyncio
 import discord
 
-client = discord.Client()
-bot = commands.Bot(command_prefix='*$')
+prefix = '*$'
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+bot = commands.Bot(command_prefix=prefix, intents=discord.Intents.all(), case_insensitive=True, self_bot=True)
 token = os.environ['DISCORD_BOT_TOKEN']
 
 # UTC
-dateTimeList = ['11:38']
+dateTimeList = ['11:00']
 
 
 @bot.event
@@ -25,15 +30,16 @@ async def time_check():
 
     check = datetime.now().strftime('%H:%M')
     now = datetime.now().strftime('%Y/%m/%d %H:%M')
-    channel = bot.get_channel(820269526733160451)
+    channel = bot.get_channel(1042432423058743356)
+    print(now)
 
     if check in dateTimeList:
         print("loopcheck2")
         if datetime.now().day % 2 == 0:
 
             print(now)
-            await channel.send('<@&559403747059695639>\n本日のギルドマイレージは\n【パターン２】\n薬草を５回採集する\n石を５回採鉱する\n木を５回伐採する\n古代遺跡５回完了\n古代遺跡１０回完了\n古代遺跡１５回完了')
+            await channel.send('@everyone\n本日のギルドマイレージは\n薬草を５回採集する\n石を５回採鉱する\n木を５回伐採する\n古代遺跡５回完了\n古代遺跡１０回完了\n古代遺跡１５回完了')
             await asyncio.sleep(60)
 
-time_check.start()
+asyncio.run(time_check())
 bot.run(token)
