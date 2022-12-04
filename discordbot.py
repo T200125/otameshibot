fa08bd294d775fa2def746615cb0b203190e3669
T200125 <80594110+T200125@users.noreply.github.com>
2022-12-04 23:19:16 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -46,6 +46,5 @@ async def time_check():
             await channel.send('@everyone\n本日のギルドマイレージは\n薬草を５回採集する\n石を５回採鉱する\n木を５回伐採する\n古代遺跡５回完了\n古代遺跡１０回完了\n古代遺跡１５回完了')
             await asyncio.sleep(60)
 
-
 asyncio.run(time_check())
 bot.run(token)
